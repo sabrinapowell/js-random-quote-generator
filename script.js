ca1599e6d902d2a1b5ca3ed75c2ee10942a4c856
Sabrina Powell <39811028+Sabby12@users.noreply.github.com>
2022-09-17 01:44:25 -0500

    fix: update styling on favourite quotes table

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ const homeTab = document.getElementById("home-tab");
 const header = document.getElementById("header");
 const loader = document.getElementById("loader");
 const tableBody = document.getElementById("table-body");
+const table = document.getElementById("favorites-table");
+const emptyTable = document.getElementById("no-favorites");
+
 
 let apiQuotes = [];
 let displayedQuoteIndexes = [];
@@ -194,36 +197,47 @@ function createAndDisplayTable() {
   let index = 0;
   removeAllTableData();
 
-  favoriteQuotes.forEach(quote => {
-    let tr = document.createElement("tr");
+  if (favoriteQuotes.length === 0) {
+    emptyTable.hidden = false;
+    table.hidden = true;
+  } else {
+    emptyTable.hidden = true;
+    table.hidden = false;
 
-    let td1 = document.createElement("td");
-    let td2 = document.createElement("td");
-    let td3 = document.createElement("td");
-
-    let text1 = document.createTextNode(quote.text);
-    let text2 = document.createTextNode(quote.author);
-
-    let icon = document.createElement("i");
-    icon.classList.add("fas");
-    icon.classList.add("fa-heart");
-    icon.classList.add("favorite-quote");
-    icon.id = index;
-    icon.addEventListener("click", function(event) {
-      removeFaveInTable(parseInt(event.target.id));
+    favoriteQuotes.forEach(quote => {
+      let tr = document.createElement("tr");
+  
+      let td1 = document.createElement("td");
+      let td2 = document.createElement("td");
+      let td3 = document.createElement("td");
+  
+      let text1 = document.createTextNode(quote.text);
+      let text2 = document.createTextNode(quote.author);
+  
+      let icon = document.createElement("i");
+      icon.classList.add("fas");
+      icon.classList.add("fa-heart");
+      icon.classList.add("favorite-quote");
+      icon.id = index;
+      icon.title = "Remove from favorites!"
+      icon.addEventListener("click", function(event) {
+        removeFaveInTable(parseInt(event.target.id));
+      });
+  
+      td1.appendChild(text1);
+      td2.appendChild(text2);
+      td3.appendChild(icon);
+  
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      tr.appendChild(td3);
+  
+      tableBody.appendChild(tr);
+      index = index + 1;
     });
+  }
 
-    td1.appendChild(text1);
-    td2.appendChild(text2);
-    td3.appendChild(icon);
-
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-
-    tableBody.appendChild(tr);
-    index = index + 1;
-  });
+  
 }
 
 function removeFaveInTable(index) {
